08bdab423cfafd17c5b7ba91b29e7c51f47f8582
David Kim <mail.ddk@gmail.com>
2017-12-03 23:17:25 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,14 @@
 'use strict';
+
+class Question {
+  constructor(question){
+    this.question = question;
+    this.save()
+  }
+  save(){
+    this.constructor._All.push(this)
+  }
+  
+}
+
+Question._All = [];
